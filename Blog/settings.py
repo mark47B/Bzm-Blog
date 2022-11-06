8359e0451d997ca8511f7c2d47163487130dd9a7
mark47B <windachae47@gmail.com>
2022-11-06 18:53:57 +0300

    Add fill SECRET_KEY from local env

--- Blog/settings.py
+++ Blog/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
+with open('.venv/secrets/SECRET_KEY') as KEY_FROM_secrets:
+    SECRET_KEY = KEY_FROM_secrets.read(-1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
