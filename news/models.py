88af7cb85308072ac19d06db7ffef7fc47e83cda
mark47B <windachae47@gmail.com>
2022-11-08 16:40:23 +0300

    Add Category model and make dependence with News model

--- news/models.py
+++ news/models.py
@@ -1,11 +1,32 @@
 from distutils.command.upload import upload
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return f'({self.pk}) {self.title}'
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['created_at']
+
+class Category(models.Model):
+    title=models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории') #  Сделали индексацию
+    
+    def __str__(self):
+        return f'{self.title}'
+    class Meta:
+        verbose_name='Категория'
+        verbose_name_plural='Категории'
+        ordering=['title']
