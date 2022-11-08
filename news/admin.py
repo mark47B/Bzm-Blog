06f2d9d45a06996be457d6265864d7bc988a0e88
mark47B <windachae47@gmail.com>
2022-11-08 16:41:17 +0300

    Registred Category and News models to Admin CMS

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import News
+from .models import Category
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published', 'category')
+    list_filter = ('is_published', 'category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', )
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
