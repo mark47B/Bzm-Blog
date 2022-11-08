311830b4302fe2a1c4f631566e4dcac470903ad0
mark47B <windachae47@gmail.com>
2022-11-08 11:36:28 +0300

    Add var description

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
-def index(request):
-    return HttpResponse('There are HTTP respnse')
+def index(request: HttpRequest):
+    return HttpResponse('There is HTTP respnse')
