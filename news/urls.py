0dad76c2b2fc5200459dacf0078efe2bb74daf22
mark47B <windachae47@gmail.com>
2022-11-11 00:43:05 +0300

    Work with Category Create category page, category view Add routing for category Add list Category at main page

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from news import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='home'),
+    path('category/<int:category_id>/', views.get_category, name='category'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,23 @@
+from re import template
+from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
-from .models import News
+from .models import News, Category
 
 def index(request: HttpRequest):
     news = News.objects.all()
-    return render(request,  'news/index.html', {'news': news, 'title': 'Список новостей'})
+    category = Category.objects.all()
+    context = {'news': news, 
+    'title': 'Список новостей',
+    'category': category
+    }
+    return render(request,  'news/index.html', context)
+
+def get_category(request: HttpRequest, category_id: int):
+    news_cat = News.objects.filter(category_id=category_id)
+    category = Category.objects.filter(pk=category_id)
+    categories = Category.objects.all()
+    context={'news_cat': news_cat,
+    'category': category,
+    'categories': categories}
+    return render(request, template_name='news/category.html', context=context)
