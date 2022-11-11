4550887fe9935f5d0888c7d1f4b79cd848277bba
mark47B <windachae47@gmail.com>
2022-11-12 00:31:55 +0300

    Add method for getting one instance of class Made a hierarchy of templates

--- news/models.py
+++ news/models.py
@@ -1,6 +1,5 @@
 from distutils.command.upload import upload
-from tabnanny import verbose
-from unicodedata import category
+from django.urls import reverse
 from django.db import models
 
 
@@ -11,7 +10,10 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)   
+
+    def get_absolute_url(self):
+        return reverse("view_news", kwargs={"news_id": self.pk})
 
     def __str__(self):
         return f'({self.pk}) {self.title}'
@@ -24,6 +26,9 @@ class News(models.Model):
 class Category(models.Model):
     title=models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории') #  Сделали индексацию
     
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"category_id": self.pk})
+
     def __str__(self):
         return f'{self.title}'
     class Meta:
--- news/views.py
+++ news/views.py
@@ -1,23 +1,29 @@
 from re import template
-from unicodedata import category
+from webbrowser import get
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from .models import News, Category
 
 def index(request: HttpRequest):
     news = News.objects.all()
-    category = Category.objects.all()
     context = {'news': news, 
-    'title': 'Список новостей',
-    'category': category
+    'title': 'Список записей'
     }
     return render(request,  'news/index.html', context)
 
 def get_category(request: HttpRequest, category_id: int):
     news_cat = News.objects.filter(category_id=category_id)
-    category = Category.objects.filter(pk=category_id)
-    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
     context={'news_cat': news_cat,
     'category': category,
-    'categories': categories}
+    'title': category.title}
     return render(request, template_name='news/category.html', context=context)
+
+def view_news(request: HttpRequest, news_id: int):
+    news_item = get_object_or_404(News, pk=news_id)
+    context = {
+        'title': news_item.title,
+        'news_item': news_item
+        }
+    return render(request, template_name='news/view_news.html', context=context)
