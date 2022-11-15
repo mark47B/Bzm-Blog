83b7e5ca57e70ef027e209fddc359633e4e007b8
mark47B <windachae47@gmail.com>
2022-11-15 11:46:49 +0300

    Make view news through DetailView class Change naming from id to pk

--- news/urls.py
+++ news/urls.py
@@ -4,8 +4,7 @@ from news import views
 urlpatterns = [
     # path('', views.index, name='home'),
     path('', views.HomeNews.as_view(), name='home'),
-    # path('category/<int:category_id>/', views.get_category, name='category'),
     path('category/<int:category_id>/', views.NewsByCategory.as_view(extra_context={'title': 'ffff'}), name='category'),
-    path('news/<int:news_id>/', views.view_news, name='view_news'),
-    path('news/add-news/', views.add_news, name='add_news'),
+    path('news/<int:pk>/', views.view_news.as_view(), name='view_news'),
+    path('news/add-news/', views.CreateNews.as_view(), name='add_news'),
 ]
