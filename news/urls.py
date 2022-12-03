c906572ff7c0991a338fa938290e71dc51e16dbb
mark47B <windachae47@gmail.com>
2022-12-03 20:42:10 +0300

    Bugfix: del garbage var

--- news/urls.py
+++ news/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('feedback/', views.feedback, name='feedback'),
     # path('', views.index, name='home'),
     path('', views.HomeNews.as_view(), name='home'),
-    path('category/<int:category_id>/', views.NewsByCategory.as_view(extra_context={'title': 'ffff'}), name='category'),
+    path('category/<int:category_id>/', views.NewsByCategory.as_view(), name='category'),
     path('news/<int:pk>/', views.view_news.as_view(), name='view_news'),
     path('news/add-news/', views.CreateNews.as_view(), name='add_news'),
 ]
